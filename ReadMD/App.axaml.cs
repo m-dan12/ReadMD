b2d1e980ca3fc31cc888e3ac7a70845bc98423fe
m-dan12 <m-dan12@yandex.ru>
2026-05-18 14:47:03 +0400

    Ссылки в md теперь открывают страницы

--- ReadMD/App.axaml.cs
+++ ReadMD/App.axaml.cs
@@ -4,12 +4,14 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using MarkView.Avalonia;
 using Microsoft.Extensions.DependencyInjection;
 using ReadMD.DI;
 using ReadMD.Services;
 using ReadMD.ViewModels;
 using ReadMD.Views;
 using System;
+using System.Diagnostics;
 using System.Linq;
 
 namespace ReadMD;
@@ -23,6 +25,9 @@ public partial class App : Application
     public override void OnFrameworkInitializationCompleted()
     {
         Services = new ServiceCollection().ConfigureServices().BuildServiceProvider();
+
+        MarkdownViewer.LinkClickedEvent.AddClassHandler<MarkdownViewer>((_, e) =>
+            Process.Start(new ProcessStartInfo(e.Url) { UseShellExecute = true }));
 
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
